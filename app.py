35ec6606b15f3efdff1ed8e0a82b3a4a54425526
mn-ju <84265754+mn-ju@users.noreply.github.com>
2021-06-05 15:02:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ ip = st.text_input("Please enter your query:","Type Here")
 op = model.predict([ip])
 if st.button('NEXT'):
  
-  st.title(f"Mr/Ms {name} of {state} state, Your query is registered in {op[0]} sector \n")
+  st.title(f"HELLO {name}! Your query is registered in {op[0]} sector \n")
   
   st.subheader(f"please contact to  toll free number:1800-180-1551 and for BSNL landline Toll-Free number: 1551 \n \n")
  
