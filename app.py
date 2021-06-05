95c614e3bcd848730b79c1c93479b4a96f86f0f6
mn-ju <84265754+mn-ju@users.noreply.github.com>
2021-06-05 13:45:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ html_temp="""
 """
 st.markdown(html_temp,unsafe_allow_html=True)
 
+
 st.subheader('Here we will help you to find the sector of your queries')
 name = st.text_input("Please enter your name:","Type Here")
 state = st.text_input("Please enter your state","Type Here")
@@ -15,11 +16,14 @@ ip = st.text_input("Please enter your query:","Type Here")
 op = model.predict([ip])
 if st.button('NEXT'):
   st.title(f"Mr/Ms {name} of {state} state, Your query is registered in {op[0]} sector")
-  st.subheader("please contact to  toll free number:1800-180-1551 and for BSNL landline Toll-Free number: 1551")
+  st.subheader("please contact to  toll free number:1800-180-1551 ")
+  st.subheader("and for BSNL landline Toll-Free number: 1551")
   
   
-st.write("This is the Machine Learning webapp of accuracy level 90%") 
-st.write("THANKYOU") 
+  
+  
+st.text("This is the Machine Learning webapp of accuracy level 90%") 
+st.text("THANKYOU") 
   
   
  
